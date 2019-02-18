f778dbd34edb8351c7e056cf7c7dbc360243b773
Stephen Weeks <tene@allalone.org>
2019-02-17 22:06:38 -0800

    Broadcast to all clients

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,19 +2,35 @@ use bytes::IntoBuf;
 use failure::Error;
 use mesh_msg::{Core, FrameEvent};
 
+use std::collections::HashMap;
+
 fn main() -> Result<(), Error> {
     let mut core = Core::new();
     let _ = core.listen("127.0.0.1:13265");
+
+    let mut clients: HashMap<usize, ()> = HashMap::new();
 
     loop {
         let frame_events = core.poll(None)?;
         for event in frame_events.into_iter() {
             use FrameEvent::*;
             match event {
-                ReceivedFrames(idx, frames) => {
+                ReceivedFrames(_idx, frames) => {
                     for frame in frames.into_iter() {
-                        core.write_frame(idx, frame.buf.into_buf());
+                        let msg = frame.buf.into_buf();
+                        for (client, _) in &clients {
+                            core.write_frame(*client, msg.clone());
+                        }
                     }
+                }
+                Accepted {
+                    listen_socket: _,
+                    conn_id,
+                } => {
+                    clients.insert(conn_id, ());
+                }
+                Closed(id) => {
+                    clients.remove(&id);
                 }
                 _ => {
                     // XX TODO
